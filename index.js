67b29df156c185f36531efb254e5dbf3873c5670
Tim Johnson <tjohn92@gmail.com>
2017-03-28 18:16:10 -0400

    added in initial files for aws

---
+++ index.js
@@ -0,0 +1,4 @@
+import lightRail from 'lightrail';
+
+// connect the alexa-app to AWS Lambda
+exports.handler = lightRail.lambda();
---
+++ lightrail.js
@@ -0,0 +1,19 @@
+import alexa from 'alexa-app';
+
+const app = new alexa.app(`Charlotte Light Rail`);
+
+app.launch((req, res) => {
+  res.say(`You've launched charlotte light rail for alexa`);
+});
+
+app.intent('GetTimeIntent', {
+    'utterances': [
+      'when is the train leaving'
+    ]
+  },
+  (req, res) => {
+    res.say(`It is leaving in 5 minutes. At 5:45 PM`);
+  }
+);
+
+export default app;
