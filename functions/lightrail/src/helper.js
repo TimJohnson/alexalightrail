1ab3135b9650a102148724d337853aae71639e97
Tim Johnson <tjohn92@gmail.com>
2017-04-16 21:51:44 -0400

    added logic for determining next outbound from carson

---
+++ functions/lightrail/src/helper.js
@@ -0,0 +1,215 @@
+import schedules from './schedules';
+
+export const blueStops = [{
+  latlng: {
+    latitude: 35.107174,
+    longitude: -80.882812
+  },
+  description: 'The end of the line.',
+  title: 'I-485 Station',
+  mapLabel: 'I-485 Station',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-1'],
+  outboundWeekday: schedules.outboundWeekday['station-15'],
+  inboundSaturday: schedules.inboundSaturday['station-1'],
+  outboundSaturday: schedules.outboundSaturday['station-15'],
+  inboundSunday: schedules.inboundSunday['station-1'],
+  outboundSunday: schedules.outboundSunday['station-15'],
+}, {
+  latlng: {
+    latitude: 35.119440,
+    longitude: -80.882333
+  },
+  title: 'Sharon Rd. West Station',
+  mapLabel: 'Sharon Rd. West',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-2'],
+  outboundWeekday: schedules.outboundWeekday['station-14'],
+  inboundSaturday: schedules.inboundSaturday['station-2'],
+  outboundSaturday: schedules.outboundSaturday['station-14'],
+  inboundSunday: schedules.inboundSunday['station-2'],
+  outboundSunday: schedules.outboundSunday['station-14'],
+}, {
+  latlng: {
+    latitude: 35.135149,
+    longitude: -80.876513
+  },
+  title: 'Arrowood Station',
+  mapLabel: 'Arrowood',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-3'],
+  outboundWeekday: schedules.outboundWeekday['station-13'],
+  inboundSaturday: schedules.inboundSaturday['station-3'],
+  outboundSaturday: schedules.outboundSaturday['station-13'],
+  inboundSunday: schedules.inboundSunday['station-3'],
+  outboundSunday: schedules.outboundSunday['station-13'],
+}, {
+  latlng: {
+    latitude: 35.152865,
+    longitude: -80.877460
+  },
+  title: 'Archdale Station',
+  mapLabel: 'Archdale',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-4'],
+  outboundWeekday: schedules.outboundWeekday['station-12'],
+  inboundSaturday: schedules.inboundSaturday['station-4'],
+  outboundSaturday: schedules.outboundSaturday['station-12'],
+  inboundSunday: schedules.inboundSunday['station-4'],
+  outboundSunday: schedules.outboundSunday['station-12'],
+}, {
+  latlng: {
+    latitude: 35.162842,
+    longitude: -80.877519
+  },
+  title: 'Tyvola Station',
+  mapLabel: 'Tyvola',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-5'],
+  outboundWeekday: schedules.outboundWeekday['station-11'],
+  inboundSaturday: schedules.inboundSaturday['station-5'],
+  outboundSaturday: schedules.outboundSaturday['station-11'],
+  inboundSunday: schedules.inboundSunday['station-5'],
+  outboundSunday: schedules.outboundSunday['station-11'],
+}, {
+  latlng: {
+    latitude: 35.175341,
+    longitude: -80.879281
+  },
+  title: 'Woodlawn Station',
+  mapLabel: 'Woodlawn',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-6'],
+  outboundWeekday: schedules.outboundWeekday['station-10'],
+  inboundSaturday: schedules.inboundSaturday['station-6'],
+  outboundSaturday: schedules.outboundSaturday['station-10'],
+  inboundSunday: schedules.inboundSunday['station-6'],
+  outboundSunday: schedules.outboundSunday['station-10'],
+}, {
+  latlng: {
+    latitude: 35.190825,
+    longitude: -80.875068
+  },
+  title: 'Scaleybark Station',
+  mapLabel: 'Scaleybark',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-7'],
+  outboundWeekday: schedules.outboundWeekday['station-9'],
+  inboundSaturday: schedules.inboundSaturday['station-7'],
+  outboundSaturday: schedules.outboundSaturday['station-9'],
+  inboundSunday: schedules.inboundSunday['station-7'],
+  outboundSunday: schedules.outboundSunday['station-9'],
+}, {
+  latlng: {
+    latitude: 35.199883,
+    longitude: -80.869027
+  },
+  title: 'New Bern Station',
+  mapLabel: 'New Bern',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-8'],
+  outboundWeekday: schedules.outboundWeekday['station-8'],
+  inboundSaturday: schedules.inboundSaturday['station-8'],
+  outboundSaturday: schedules.outboundSaturday['station-8'],
+  inboundSunday: schedules.inboundSunday['station-8'],
+  outboundSunday: schedules.outboundSunday['station-8'],
+}, {
+  latlng: {
+    latitude: 35.212099,
+    longitude: -80.859134
+  },
+  title: 'East/West Station',
+  mapLabel: 'East/West',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-9'],
+  outboundWeekday: schedules.outboundWeekday['station-7'],
+  inboundSaturday: schedules.inboundSaturday['station-9'],
+  outboundSaturday: schedules.outboundSaturday['station-7'],
+  inboundSunday: schedules.inboundSunday['station-9'],
+  outboundSunday: schedules.outboundSunday['station-7'],
+}, {
+  latlng: {
+    latitude: 35.215771,
+    longitude: -80.855336
+  },
+  title: 'Bland Street Station',
+  mapLabel: 'Bland Street',
+  labelSide: 'left',
+  inboundWeekday: schedules.inboundWeekday['station-10'],
+  outboundWeekday: schedules.outboundWeekday['station-6'],
+  inboundSaturday: schedules.inboundSaturday['station-10'],
+  outboundSaturday: schedules.outboundSaturday['station-6'],
+  inboundSunday: schedules.inboundSunday['station-10'],
+  outboundSunday: schedules.outboundSunday['station-6'],
+}, {
+  latlng: {
+    latitude: 35.218808,
+    longitude: -80.850954
+  },
+  title: 'Carson Station',
+  mapLabel: 'Carson',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-11'],
+  outboundWeekday: schedules.outboundWeekday['station-5'],
+  inboundSaturday: schedules.inboundSaturday['station-11'],
+  outboundSaturday: schedules.outboundSaturday['station-5'],
+  inboundSunday: schedules.inboundSunday['station-11'],
+  outboundSunday: schedules.outboundSunday['station-5'],
+}, {
+  latlng: {
+    latitude: 35.221313,
+    longitude: -80.847064
+  },
+  title: 'Stonewall Station',
+  mapLabel: 'Stonewall',
+  labelSide: 'left',
+  inboundWeekday: schedules.inboundWeekday['station-12'],
+  outboundWeekday: schedules.outboundWeekday['station-4'],
+  inboundSaturday: schedules.inboundSaturday['station-12'],
+  outboundSaturday: schedules.outboundSaturday['station-4'],
+  inboundSunday: schedules.inboundSunday['station-12'],
+  outboundSunday: schedules.outboundSunday['station-4'],
+}, {
+  latlng: {
+    latitude: 35.223630,
+    longitude: -80.843222
+  },
+  title: '3rd Street Station',
+  mapLabel: '3rd Street',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-13'],
+  outboundWeekday: schedules.outboundWeekday['station-3'],
+  inboundSaturday: schedules.inboundSaturday['station-13'],
+  outboundSaturday: schedules.outboundSaturday['station-3'],
+  inboundSunday: schedules.inboundSunday['station-13'],
+  outboundSunday: schedules.outboundSunday['station-3'],
+}, {
+  latlng: {
+    latitude: 35.225298,
+    longitude: -80.840909
+  },
+  title: 'Charlotte Transportation Center',
+  mapLabel: 'CTC/Arena',
+  labelSide: 'left',
+  inboundWeekday: schedules.inboundWeekday['station-14'],
+  outboundWeekday: schedules.outboundWeekday['station-2'],
+  inboundSaturday: schedules.inboundSaturday['station-14'],
+  outboundSaturday: schedules.outboundSaturday['station-2'],
+  inboundSunday: schedules.inboundSunday['station-14'],
+  outboundSunday: schedules.outboundSunday['station-2'],
+}, {
+  latlng: {
+    latitude: 35.227370,
+    longitude: -80.838119
+  },
+  description: 'The other end of the line.',
+  title: '7th Street Station',
+  mapLabel: '7th Street',
+  labelSide: 'right',
+  inboundWeekday: schedules.inboundWeekday['station-15'],
+  outboundWeekday: schedules.outboundWeekday['station-1'],
+  inboundSaturday: schedules.inboundSaturday['station-15'],
+  outboundSaturday: schedules.outboundSaturday['station-1'],
+  inboundSunday: schedules.inboundSunday['station-15'],
+  outboundSunday: schedules.outboundSunday['station-1'],
+}]
--- functions/lightrail/src/lightrail.js
+++ functions/lightrail/src/lightrail.js
@@ -1,4 +1,6 @@
 import alexa from 'alexa-app';
+import moment from 'moment';
+import { blueStops } from './helper.js'
 
 const app = new alexa.app(`charlotteLightRail`);
 
@@ -12,8 +14,55 @@ app.intent('GetTimeIntent', {
     ]
   },
   (req, res) => {
-    res.say(`It is leaving in 5 minutes. At 5:45 PM`);
+
+    //hard coded for outbound from carson station
+    let nextTime = getNextTrainTime('outbound', 11);
+    console.log('next time: ', nextTime);
+
+    res.say(`The next outbound train is leaving carson station at ${nextTime.time}`);
   }
 );
 
+let getNextTrainTime = (direction, stationIndex) => {
+  const targetStation = blueStops[stationIndex]
+
+  let day = moment().day()
+  let time = moment().format('HH:mm')
+
+  const getTime = (firstOrLast, dayOfWeek) => {
+    let getTime = targetStation[`${direction}${dayOfWeek}`]
+      .filter(time => time !== 'no stop')
+    if (firstOrLast === 'last' && getTime[getTime.length - 1] < '12:00') {
+      return getTime[getTime.length - 1]
+    } else if (firstOrLast === 'last') {
+      return '00:00'
+    }
+    return getTime[0]
+  }
+
+  // Based on current time and day of week, determine which schedule should be in effect.
+  let targetSchedule
+  if ((day === 6 && time > getTime('last', 'Weekday')) || (day === 0 && time <= getTime('first', 'Sunday'))) {
+    targetSchedule = `${direction}Saturday`
+  } else if ((day === 0 && time > getTime('last', 'Saturday')) || (day === 1 && time <= getTime('first', 'Weekday'))) {
+    targetSchedule = `${direction}Sunday`
+  } else {
+    targetSchedule = `${direction}Weekday`
+  }
+
+  // Filter out the 'no stops' and sort from earliest time to latest so that we can get the next train time.
+  const schedule = targetStation[targetSchedule]
+    .filter(time => time !== 'no stop')
+    .sort()
+
+  const firstTrain = moment(schedule[0], 'HH:mm')
+  const nextTrain = schedule
+    .map(time => moment(time, 'HH:mm'))
+    .find(time => time.isAfter(moment()))
+
+  // If there's no scheduled time after the current time, then default to the first time
+  const nextTrainTime = nextTrain ? nextTrain.format('LT') : firstTrain.format('LT')
+  const nextTrainDelta = nextTrain ? nextTrain.fromNow() : firstTrain.add(1, 'days').fromNow()
+  return { time: nextTrainTime, delta: nextTrainDelta }
+}
 export default app;
